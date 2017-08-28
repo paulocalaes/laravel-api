1d839b4cdf49014058f5dd97db980162f68590dc
Paulo Calaes <paulocalaes@gmail.com>
2017-08-28 11:43:35 -0300

    1st version

---
+++ laravel-api/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(array('prefix' => 'api'), function()
+{
+
+  Route::get('/', function () {
+      return response()->json(['message' => 'Jobs API', 'status' => 'Connected']);;
+  });
+
+  Route::resource('jobs', 'JobsController');
+  Route::resource('companies', 'CompaniesController');
+});
+
+Route::get('/', function () {
+    return redirect('api');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
